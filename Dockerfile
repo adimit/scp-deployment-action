a06632322ceea18492fbe15348f893ac6be84bc3
Aleksandar Dimitrov <git@aleks.bg>
2020-10-06 22:24:57 +0200

    Test version

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM alpine:3.12
 RUN apk add rsync openssh-client
 
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+# Set up SSH secrets
+test -n $ELLIPTIC_KEY || (echo "Need elliptic key" && exit 1)
+test -n $HOST_KEY || (echo "Need host key" && exit 1)
+
+SSH_PATH=$HOME/.ssh
+mkdir -p $SSH_PATH
+echo "$ELLIPTIC_KEY" > $SSH_PATH/id_ed25519
+echo "$HOST_ENTRY" > $SSH_PATH/known_hosts
+chmod 400 $SSH_PATH/*
+
+env
+
+ls -R $GITHUB_WORKTREE
+
+cat $SSH_PATH/*
+
+echo $*
+echo $@
+
