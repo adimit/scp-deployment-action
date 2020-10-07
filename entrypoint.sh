3a79ef6d15dc30d74694ab7682eedb7220e4d2ec
Aleksandar Dimitrov <git@aleks.bg>
2020-10-07 22:01:34 +0200

    brute force

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ HOST_ENTRY=$5
 SSH_PATH=$HOME/.ssh
 mkdir -p $SSH_PATH
 echo "$ELLIPTIC_KEY" > $SSH_PATH/id_ed25519
-echo "$HOST_ENTRY" > $SSH_PATH/known_hosts
+# echo "$HOST_ENTRY" > $SSH_PATH/known_hosts
 chmod 600 $SSH_PATH/*
 
-md5sum $SSH_PATH/known_hosts
+ssh-keyscan pryslopska.com > $SSH_PATH/known_hosts
 
 rsync -rvult . $SSH_USER@$SSH_HOST:$HOST_PATH
