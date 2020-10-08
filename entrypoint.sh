9a281695933887dab1d861910283cea216d1d676
Aleksandar Dimitrov <git@aleks.bg>
2020-10-08 07:50:02 +0200

    Fix host key path

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ HOST_ENTRY=$5
 SSH_PATH=/root/.ssh
 mkdir -p $SSH_PATH
 echo "$ELLIPTIC_KEY" > $SSH_PATH/id_ed25519
-echo "$HOST_ENTRY" > $SSH_PATH/ssh_known_hosts
+echo "$HOST_ENTRY" > $SSH_PATH/known_hosts
 chmod 600 $SSH_PATH/*
 
 rsync -rvulti --exclude .git --delete-after . $SSH_USER@$SSH_HOST:$HOST_PATH
