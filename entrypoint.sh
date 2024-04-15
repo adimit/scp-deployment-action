f0cd66d7ab2e3f8747086f65581c50b2dc373da0
Aleksandar Dimitrov <git@aleks.bg>
2024-04-15 23:29:30 +0200

    Add possibility to define a custom SSH port

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ SSH_USER=$2
 HOST_PATH=$3
 ELLIPTIC_KEY=$4
 HOST_ENTRY=$5
+SSH_PORT=${6:-22}
 
 SSH_PATH=/root/.ssh
 mkdir -p $SSH_PATH
@@ -12,4 +13,4 @@ echo "$ELLIPTIC_KEY" > $SSH_PATH/id_ed25519
 echo "$HOST_ENTRY" > $SSH_PATH/known_hosts
 chmod 600 $SSH_PATH/*
 
-rsync -rvulti --exclude .git --delete-after . $SSH_USER@$SSH_HOST:$HOST_PATH
+rsync -rvulti -e "ssh -p $SSH_PORT" --exclude .git --delete-after . $SSH_USER@$SSH_HOST:$HOST_PATH
