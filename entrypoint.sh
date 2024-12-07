12885f88d162d02b6a101adcd198d07e00ac0029
Aleksandar Dimitrov <git@aleks.bg>
2024-12-07 17:37:25 +0100

    Add LOCAL_PATH setting

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ HOST_PATH=$3
 ELLIPTIC_KEY=$4
 HOST_ENTRY=$5
 SSH_PORT=${6:-22}
+LOCAL_PATH=${7:-.}
 
 SSH_PATH=/root/.ssh
 mkdir -p $SSH_PATH
@@ -13,4 +14,4 @@ echo "$ELLIPTIC_KEY" > $SSH_PATH/id_ed25519
 echo "$HOST_ENTRY" > $SSH_PATH/known_hosts
 chmod 600 $SSH_PATH/*
 
-rsync -rvulti -e "ssh -p $SSH_PORT" --exclude .git --delete-after . $SSH_USER@$SSH_HOST:$HOST_PATH
+rsync -rvulti -e "ssh -p $SSH_PORT" --exclude .git --delete-after "$LOCAL_PATH" "$SSH_USER"@"$SSH_HOST":"$HOST_PATH"
